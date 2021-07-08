fdb336615f4f45c6a937e050c96274effb513e83
kouheiadachi <kohei.adachi@blueish.co.jp>
2021-07-08 17:43:54 +0900

    認証実装

--- backend/app/Providers/AuthServiceProvider.php
+++ backend/app/Providers/AuthServiceProvider.php
@@ -4,6 +4,11 @@ namespace App\Providers;
 
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 use Illuminate\Support\Facades\Gate;
+// use App\Services\Auth\CognitoGuard;
+// use App\Services\Cognito\JWTVerifier;
+use App\Services\Auth;
+use App\Auth\CognitoGuard;
+use App\Auth\JWTVerifier;
 
 class AuthServiceProvider extends ServiceProvider
 {
@@ -23,8 +28,16 @@ class AuthServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        // $this->registerPolicies();
+
         $this->registerPolicies();
 
-        //
+        $this->app['auth']->extend('cognito', function($app, $name, array $config) {
+            return new CognitoGuard(
+                new JWTVerifier(),
+                $app['request'],
+                $app['auth']->createUserProvider($config['provider'])
+            );
+        });
     }
 }
